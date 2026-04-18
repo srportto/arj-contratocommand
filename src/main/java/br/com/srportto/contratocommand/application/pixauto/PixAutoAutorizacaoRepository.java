4f76d29f68b927fbf7c9b772c01d57fc354a6572
srportto <ceeh1100@gmail.com>
2026-04-18 18:26:58 -0300

    ajuste id entidade autorizacao no repository

--- src/main/java/br/com/srportto/contratocommand/application/pixauto/PixAutoAutorizacaoRepository.java
+++ src/main/java/br/com/srportto/contratocommand/application/pixauto/PixAutoAutorizacaoRepository.java
@@ -4,12 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.UUID;
 
 import br.com.srportto.contratocommand.domain.entities.Autorizacao;
+import br.com.srportto.contratocommand.domain.entities.IdAutorizacao;
 
 @Repository
-public interface PixAutoAutorizacaoRepository extends JpaRepository<Autorizacao, UUID> {
+public interface PixAutoAutorizacaoRepository extends JpaRepository<Autorizacao, IdAutorizacao> {
 
   List<Autorizacao> findByStatus(Integer status);
 
